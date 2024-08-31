fbd96f3599cbecd9ffa0bce4dc61a9fb2902bca9
MatiasNuniez <matiasnuniez192@gmail.com>
2024-08-31 10:41:36 -0300

    Product&ProviderModels

---
+++ server/src/interfaces/productInterface.ts
@@ -0,0 +1,6 @@
+export interface productInterface {
+    nombre:string;
+    descripcion:string;
+    costo:number;
+    precio_u:number;
+}
--- server/src/models/productsModel.ts
+++ server/src/models/productsModel.ts
@@ -1,5 +1,31 @@
-import { Schema } from "mongoose";
+import mongoose from "mongoose";
+import { productInterface } from "../interfaces/productInterface";
 
 class ProductModel {
-    
+    public _model: mongoose.Model<productInterface>
+
+    constructor(){
+        const productModel = new mongoose.Schema<productInterface>({
+            nombre:{
+                type:String,
+                require:true
+            },
+            descripcion:{
+                type:String,
+                required:true,
+                minlength:10
+            },
+            costo:{
+                type:Number,
+                required:true
+            },
+            precio_u:{
+                type:Number,
+                required:true
+            }
+        })
+        this._model = mongoose.model('ProductModel', productModel)
+    }
 }
+
+export const ModelProduct = new ProductModel()._model; 
