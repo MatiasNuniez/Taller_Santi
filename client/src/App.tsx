8003b9768d5e494cc9b452a9c2554f8cb34e165d
MatiasNuniez <matiasnuniez192@gmail.com>
2024-11-08 20:19:54 -0300

    ubunutu fin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import Login from './components/Login/Login';
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import Register from './components/Register/Register';
 import Index from './components/Index';
-import ProductDetails from './components/ProductDetails/ProductDetails';
 import Header from './components/Header/Header';
 import Dashboard from './components/Dashboard/Dashboard';
 import AddProduct from './components/Product/AddProduct';
@@ -24,8 +23,6 @@ function App() {
           <Route path="/register" Component={ Register } />
 
           <Route path='/' Component={Index}/>
-
-          <Route path='/details/:id' Component={ProductDetails}/>
 
           <Route path='/dashboard' Component={Dashboard}/>
 
--- client/src/components/Index/index.tsx
+++ client/src/components/Index/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { productsInterface } from "../../interfaces/Interfaces";
-import Modal from './Modal'; // Asegúrate de tener el modal en el mismo directorio o ajustar la ruta de importación.
+import Modal from './Modal';
 
 const Index: React.FC = () => {
   const [products, setProducts] = useState<Array<productsInterface>>([]);
@@ -9,8 +9,17 @@ const Index: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState<productsInterface | null>(null);
   const [buscado, setBuscado] = useState<string>('');
-  const [filterProducts, setFilterProducts] = useState<Array<productsInterface>>([]);
 
+  // Función de filtrado que recibe productos y el término de búsqueda
+  const filtrado = (character: Array<productsInterface>, buscado: string) => {
+    if (!buscado) return character;
+    return character.filter((p) => p.nombre.toLowerCase().includes(buscado.toLowerCase()));
+  };
+
+  // Asignacion de los resultados a la variable para poder mapearlo cuando se cambie el valor.
+  const filterProducts = filtrado(products, buscado);
+
+  // Peticion a la api para obtener todos los productos en la base de datos.
   const getProducts = async () => {
     try {
       const res = await fetch(`http://localhost:3000/api/products/${userDNI}`, {
@@ -30,9 +39,10 @@ const Index: React.FC = () => {
     }
   };
 
+  // Funcion para agregar productos al carrito
   const addToCart = (newProduct: productsInterface) => {
     const cartStored = localStorage.getItem('cart');
-    
+
     let updatedCart = [];
 
     if (cartStored) {
@@ -45,48 +55,45 @@ const Index: React.FC = () => {
     localStorage.setItem('cart', JSON.stringify(updatedCart));
   };
 
+  // Funcion para remover un dato de la base de datos y localstorage
   const removeToDDBB = async (id: string) => {
     try {
-      const deleteProduct = await fetch(`http://localhost:3000/api/deleteProduct/${id}/${userDNI}`,{
-        method:'DELETE'
+      const deleteProduct = await fetch(`http://localhost:3000/api/deleteProduct/${id}/${userDNI}`, {
+        method: 'DELETE'
       })
-      if(deleteProduct.ok){
+      if (deleteProduct.ok) {
         setProducts(products.filter((product) => product._id !== id));
       }
     } catch (error) {
       throw new Error('Error al eliminar elemento');
     }
   };
-
+  // Funcion para pasarle al modal para poder llevar los datos del producto que quiero editar.
   const handleEdit = (product: productsInterface) => {
     setSelectedProduct(product);
-    setIsModalOpen(true); // Abre el modal
+    setIsModalOpen(true);
   };
 
+  // Funcion para actualizar los datos de los productos actualizados
   const handleSave = (updatedProduct: any) => {
-    // Aquí puedes hacer una llamada a la API para actualizar el producto en la base de datos.
     const updatedProducts = products.map((product) =>
       product._id === updatedProduct._id ? updatedProduct : product
     );
     setProducts(updatedProducts);
-    setIsModalOpen(false); // Cierra el modal
+    setIsModalOpen(false);
   };
 
+  // Funcion para actualizar el buscado cada vez que se cambie el valor del input.
   const handleBusc = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBuscado(e.target.value);
+    const searchValue = e.target.value;
+    setBuscado(searchValue);
   };
 
+  // Hook que se ejecuta cada vez que se renderiza la pagina
   useEffect(() => {
     getProducts();
     localStorage.setItem('cart', '');
-    if (buscado) {
-      setFilterProducts(products.filter((p) =>
-        p.nombre.toLowerCase().includes(buscado.toLowerCase())
-      ));
-    } else {
-      setFilterProducts(products); // Si no hay búsqueda, mostrar todos los productos
-    }
-  }, [userDNI, buscado, products]);
+  }, [userDNI]);
 
   return (
     <div className="p-6">
@@ -113,21 +120,28 @@ const Index: React.FC = () => {
               <div className="p-4">
                 <h3 className="text-lg font-bold text-gray-800">{product.nombre}</h3>
                 <p className="text-gray-600 text-sm">{product.descripcion}</p>
-                <div className="mt-4 flex justify-between items-center">
-                  <span className="text-xl font-bold text-tobacco">${product.precio_u}</span>
-                  <button className="bg-red-700 text-white px-4 py-2 rounded-md" onClick={() => removeToDDBB(product._id)}>
+                <div className="mt-4 flex justify-between items-center space-x-2">
+                  <span className="text-2xl font-semibold text-tobacco">${product.precio_u}</span>
+                  <button
+                    className="bg-red-600 text-white px-3 py-1 rounded-full hover:bg-red-500 transition-colors"
+                    onClick={() => removeToDDBB(product._id)}
+                  >
                     Eliminar
                   </button>
                   <button
-                    className="bg-yellow-500 text-white px-4 py-2 rounded-md"
+                    className="bg-yellow-400 text-white px-3 py-1 rounded-full hover:bg-yellow-300 transition-colors"
                     onClick={() => handleEdit(product)}
                   >
                     Editar
                   </button>
-                  <button className="bg-tobacco text-white px-4 py-2 rounded-md" onClick={() => addToCart(product)}>
-                    Añadir
+                  <button
+                    className="bg-tobacco text-white px-3 py-1 rounded-full hover:bg-opacity-80 transition-opacity"
+                    onClick={() => addToCart(product)}
+                  >
+                    +Carrito
                   </button>
                 </div>
+
               </div>
             </div>
           ))
@@ -135,7 +149,7 @@ const Index: React.FC = () => {
           <p className="col-span-full text-center text-gray-500">No se encontraron productos</p>
         )}
       </div>
-  
+
       {isModalOpen && selectedProduct && (
         <Modal
           onClose={() => setIsModalOpen(false)}
@@ -146,7 +160,7 @@ const Index: React.FC = () => {
       )}
     </div>
   );
-  
+
 };
 
 export default Index;
