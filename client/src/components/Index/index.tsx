5b6837ab372d560f64991d92c558e002fb34dd9e
Matias Nunez <matiasnuniez192@gmail.com>
2024-11-22 19:38:50 -0300

    redirect login

--- client/src/components/Index/index.tsx
+++ client/src/components/Index/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { productsInterface } from "../../interfaces/Interfaces";
 import Modal from './Modal';
+import { useNavigate } from "react-router-dom"; // Asegúrate de tener instalado react-router-dom
 
 const Index: React.FC = () => {
   const [products, setProducts] = useState<Array<productsInterface>>([]);
@@ -10,6 +11,7 @@ const Index: React.FC = () => {
   const [buscado, setBuscado] = useState<string>('');
   const [token, setToken] = useState<string>('');
   const [rol, setRol] = useState<string>('');
+  const navigate = useNavigate(); // Hook para navegación
 
   // Función de filtrado que recibe productos y el término de búsqueda
   const filtrado = (character: Array<productsInterface>, buscado: string) => {
@@ -73,116 +75,117 @@ const Index: React.FC = () => {
     setBuscado(searchValue);
   };
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem('sesiontoken');
-    if (storedToken) {
-      setToken(storedToken);
-    }
-
-    const storedRol = localStorage.getItem('rolxtabaqueria');
-    setRol(storedRol === 'admin' ? 'admin' : 'empleado');
-    localStorage.setItem('cart', '');
-  }, []);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      if (!token) return; // No intentar la petición si el token aún no está disponible
-
-      try {
-        const res = await fetch(`http://localhost:3000/api/products`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-          }
-        });
-
-        if (!res.ok) {
-          throw new Error('No se completó la petición');
-        }
-
-        const data = await res.json();
-        if (data.length > 0) {
-          setProducts(data);
-        } else {
-          console.warn('No se encontraron productos en la base de datos.');
-        }
-      } catch (error) {
-        console.error('Error al consultar a la base de datos desde getProductos', error);
+    useEffect(() => {
+      const storedToken = localStorage.getItem('sesiontoken');
+      if (storedToken) {
+        setToken(storedToken);
+      } else {
+        navigate('/login'); 
       }
-    };
+      const storedRol = localStorage.getItem('rolxtabaqueria');
+      setRol(storedRol === 'admin' ? 'admin' : 'empleado');
+      localStorage.setItem('cart', '');
+    }, []);
 
-    fetchProducts();
-  }, [token]);
+    useEffect(() => {
+      const fetchProducts = async () => {
+        if (!token) return;
 
-  return (
-    <div className="p-6">
-      <div className="mb-4">
-        <input
-          type="text"
-          onChange={(e) => handleBusc(e)}
-          placeholder='Buscar...'
-          className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-tobacco transition duration-200"
-        />
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {filterProducts.length > 0 ? (
-          filterProducts.map((product) => (
-            <div
-              key={product._id}
-              className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
-            >
-              <img
-                src={product.img}
-                alt={product.nombre}
-                className="w-full h-48 object-contain"
-              />
-              <div className="p-4">
-                <h3 className="text-lg font-bold text-gray-800">{product.nombre}</h3>
-                <p className="text-gray-600 text-sm">{product.descripcion}</p>
-                <div className="mt-4 flex justify-between items-center space-x-2">
-                  <span className="text-2xl font-semibold text-tobacco">${product.precio_u}</span>
-                  {rol === 'admin' ?
+        try {
+          const res = await fetch(`http://localhost:3000/api/products`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            }
+          });
+
+          if (!res.ok) {
+            throw new Error('No se completó la petición');
+          }
+
+          const data = await res.json();
+          if (data.length > 0) {
+            setProducts(data);
+          } else {
+            console.warn('No se encontraron productos en la base de datos.');
+          }
+        } catch (error) {
+          console.error('Error al consultar a la base de datos desde getProductos', error);
+        }
+      };
+
+      fetchProducts();
+    }, [token]);
+
+    return (
+      <div className="p-6">
+        <div className="mb-4">
+          <input
+            type="text"
+            onChange={(e) => handleBusc(e)}
+            placeholder='Buscar...'
+            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-tobacco transition duration-200"
+          />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {filterProducts.length > 0 ? (
+            filterProducts.map((product) => (
+              <div
+                key={product._id}
+                className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
+              >
+                <img
+                  src={product.img}
+                  alt={product.nombre}
+                  className="w-full h-48 object-contain"
+                />
+                <div className="p-4">
+                  <h3 className="text-lg font-bold text-gray-800">{product.nombre}</h3>
+                  <p className="text-gray-600 text-sm">{product.descripcion}</p>
+                  <div className="mt-4 flex justify-between items-center space-x-2">
+                    <span className="text-2xl font-semibold text-tobacco">${product.precio_u}</span>
+                    {rol === 'admin' ?
+                      <button
+                        className="bg-red-600 text-white px-3 py-1 rounded-full hover:bg-red-500 transition-colors"
+                        onClick={() => removeToDDBB(product._id)}
+                      >
+                        Eliminar
+                      </button>
+                      : null
+                    }
                     <button
-                      className="bg-red-600 text-white px-3 py-1 rounded-full hover:bg-red-500 transition-colors"
-                      onClick={() => removeToDDBB(product._id)}
+                      className="bg-yellow-400 text-white px-3 py-1 rounded-full hover:bg-yellow-300 transition-colors"
+                      onClick={() => handleEdit(product)}
                     >
-                      Eliminar
+                      Editar
                     </button>
-                    : null
-                  }
-                  <button
-                    className="bg-yellow-400 text-white px-3 py-1 rounded-full hover:bg-yellow-300 transition-colors"
-                    onClick={() => handleEdit(product)}
-                  >
-                    Editar
-                  </button>
-                  <button
-                    className="bg-tobacco text-white px-3 py-1 rounded-full hover:bg-opacity-80 transition-opacity"
-                    onClick={() => addToCart(product)}
-                  >
-                    +Carrito
-                  </button>
-                </div>
+                    <button
+                      className="bg-tobacco text-white px-3 py-1 rounded-full hover:bg-opacity-80 transition-opacity"
+                      onClick={() => addToCart(product)}
+                    >
+                      +Carrito
+                    </button>
+                  </div>
 
+                </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <p className="col-span-full text-center text-gray-500">No se encontraron productos</p>
+            ))
+          ) : (
+            <p className="col-span-full text-center text-gray-500">No se encontraron productos</p>
+          )}
+        </div>
+
+        {isModalOpen && selectedProduct && (
+          <Modal
+            onClose={() => setIsModalOpen(false)}
+            onSave={handleSave}
+            initialData={selectedProduct}
+          />
         )}
       </div>
+    );
 
-      {isModalOpen && selectedProduct && (
-        <Modal
-          onClose={() => setIsModalOpen(false)}
-          onSave={handleSave}
-          initialData={selectedProduct} // Pasamos el producto seleccionado al modal
-        />
-      )}
-    </div>
-  );
+  };
 
-};
-
-export default Index;
+  export default Index;
